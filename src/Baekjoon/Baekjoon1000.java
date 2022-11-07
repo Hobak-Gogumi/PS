37b35f01f33830c4dcf2583ead64f7c71ff64b35
SEOHYEON LEE <seoooohs2@naver.com>
2022-11-07 11:25:43 +0900

    [Baekjoon] 1000 - A+B

---
+++ src/Baekjoon/Baekjoon1000.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon1000 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        System.out.println(n1 + n2);
+    }
+}
