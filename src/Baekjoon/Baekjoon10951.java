a6536a89c817fae211f92f55cf22b3fe3b985555
SEOHYEON LEE <seoooohs2@naver.com>
2022-12-08 09:47:39 +0900

    [Baekjoon] 10951 - A+B - 4

---
+++ src/Baekjoon/Baekjoon10951.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10951 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            System.out.println(A+B);
+        }
+    }
+}
