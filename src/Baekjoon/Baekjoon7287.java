b051a83845677b88cc10cd0236ef5cd063573640
SEOHYEON LEE <seoooohs2@naver.com>
2022-12-17 11:16:39 +0900

    [Baekjoon] 7287 - 등록

---
+++ src/Baekjoon/Baekjoon7287.java
@@ -0,0 +1,10 @@
+package Baekjoon;
+
+public class Baekjoon7287 {
+
+    public static void main(String[] args) {
+        System.out.println("49");
+        System.out.println("eo_oe17");
+    }
+
+}
