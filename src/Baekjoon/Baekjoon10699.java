de7a360777ec4c2ef3e8e3faa849cbc16e51f827
SEOHYEON LEE <seoooohs2@naver.com>
2022-12-18 11:19:16 +0900

    [Baekjoon] 10699 - 오늘 날짜

---
+++ src/Baekjoon/Baekjoon10699.java
@@ -0,0 +1,9 @@
+package Baekjoon;
+
+import java.time.LocalDate;
+
+public class Baekjoon10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
