603733a3d4ec316e8083ac190ac55edc538958b1
SEOHYEON LEE <seoooohs2@naver.com>
2023-03-13 09:31:58 +0900

    [Programmers] 두 수의 곱

--- src/Programmers/두_수의_곱.java
+++ src/Programmers/두_수의_곱.java
@@ -1,5 +1,10 @@
-class Solution {
-    public int solution(int num1, int num2) {
-        return num1 * num2;
+package Programmers;
+
+public class 두_수의_곱 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 * num2;
+        }
     }
 }
