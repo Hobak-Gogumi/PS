c855772427bebe609912f8376df1e0f55be70e60
SEOHYEON LEE <seoooohs2@naver.com>
2023-03-14 09:34:20 +0900

    [Programmers] 자릿수 더하기

---
+++ src/Programmers/자릿수_더하기.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+public class 자릿수_더하기 {
+
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+
+            while(n != 0){
+                answer += (n % 10);
+                n = n / 10;
+            }
+
+            return answer;
+        }
+    }
+
+}
