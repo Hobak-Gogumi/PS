f847f7eb953a419796f5bc206a261b1fd641ddcc
SEOHYEON LEE <seoooohs2@naver.com>
2023-05-07 20:06:42 +0900

    [Programmers] 나머지 구하기

---
+++ src/Programmers/나머지_구하기.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+public class 나머지_구하기 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 % num2;
+        }
+    }
+    
+}
