72ede62fa27f3d43b3e78e7b61059ec061a9225e
SEOHYEON LEE <seoooohs2@naver.com>
2023-05-15 10:55:01 +0900

    [Programmers] 숫자 비교하기

---
+++ src/Programmers/숫자_비교하기.java
@@ -0,0 +1,14 @@
+package Programmers;
+
+public class 숫자_비교하기 {
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            if(num1 == num2)
+                return 1;
+            else
+                return -1;
+        }
+    }
+
+}
