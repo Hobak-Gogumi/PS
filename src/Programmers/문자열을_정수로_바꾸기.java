1d3e5c9e08d3add63213fde9a5650125808ace17
SEOHYEON LEE <seoooohs2@naver.com>
2023-05-16 10:07:42 +0900

    [Programmers] 문자열을 정수로 바꾸기

---
+++ src/Programmers/문자열을_정수로_바꾸기.java
@@ -0,0 +1,12 @@
+package Programmers;
+
+public class 문자열을_정수로_바꾸기 {
+
+    class Solution {
+        public int solution(String s) {
+            int answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+
+}
