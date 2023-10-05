3fd77f65ea21aaeb42ec45dc8db231275bf3f540
SEOHYEON LEE <seoooohs2@naver.com>
2023-10-05 23:03:07 +0900

    [Baekjoon] 16430 - 제리와 톰

--- src/Baekjoon/Baekjoon16430.java
+++ src/Baekjoon/Baekjoon16430.java
@@ -15,6 +15,7 @@ public class Baekjoon16430 {
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
+        //A와 B가 서로소임이 보장되므로, 약분을 할 필요가 없다. 단순 뺄셈만 해주면 된다.
         sb.append(B-A).append(" ").append(B).append("\n");
 
         bw.write(sb.toString());
