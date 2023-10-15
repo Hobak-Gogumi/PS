4e4587e075981edfc873d1ce2c7e18183b8a6c31
SEOHYEON LEE <seoooohs2@naver.com>
2023-10-15 20:52:28 +0900

    [Baekjoon] 9316 - Hello Judge

---
+++ src/Baekjoon/Baekjoon9316.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.io.*;
+
+public class Baekjoon9316 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            sb.append("Hello World, Judge ").append(i).append("!\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+}
