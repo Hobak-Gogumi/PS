e4b8fb78d01402a36fa4c4fb28aec27021d1f749
SEOHYEON LEE <seoooohs2@naver.com>
2023-11-20 23:35:44 +0900

    [Baekjoon] 15680 - 연세대학교

---
+++ src/Baekjoon/Baekjoon15680.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.io.*;
+
+public class Baekjoon15680 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N == 0){
+            bw.write("YONSEI");
+        } else if(N == 1){
+            bw.write("Leading the Way to the Future");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+}
